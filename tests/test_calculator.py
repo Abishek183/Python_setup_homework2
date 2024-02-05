4158483c130ea383918cdaa51beffbd5efbf041d
Abishek183 <av739@njit.edu>
2024-02-04 20:48:33 -0500

    calculator with class and methods

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,19 +1,9 @@
-'''My Calculator Test'''
-from calculator import add, subtract, multiply, divide
+'''Testing the calculator class'''
+from calculator import calculator
 
-def test_addition():
-    '''Test that addition function works '''    
-    assert add(2,2) == 4
-
-def test_subtraction():
-    '''Test that subtraction function works '''    
-    assert subtract(2,2) == 0
-
-def test_multiplication():
-    '''Test that multiplication function works '''    
-    assert multiply(2,3) == 6
-
-def test_divition():
-    '''Test that divition function works '''    
-    assert divide(2,2) == 1
-    
+def test_calculator():
+    '''Testing the calculator methods'''
+    assert calculator.add(2,2) == 4
+    assert calculator.subtract(2,2) == 0
+    assert calculator.multiply(2,3) == 6
+    assert calculator.divide(2,2) == 1
